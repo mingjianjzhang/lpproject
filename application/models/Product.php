ffed00e3fd04a2712d2fb45af77fe9d67a380b60
Timothy Lo <tim7643@gmail.com>
2016-09-28 11:30:03 -0700

    edited some user models

--- application/models/Product.php
+++ application/models/Product.php
@@ -2,9 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Product extends CI_Model {
-	public function getItemDetails() {
-		$sql="SELECT name, price, img, description FROM products WHERE";
-
+	public function getItemDetails($id) {
+		$sql="SELECT name, price, img, description FROM products WHERE id = $id";
+		return $this->db->query($sql);
 	}
 	public function getSimilarItems() {
 		// get pic name and price for items in the same category
--- application/views/cart.php
+++ application/views/cart.php
@@ -52,10 +52,6 @@ $this->load->view('partials/userHeader');
 		<input type="text" class="form-control" placeholder="Address" aria-describedby="basic-addon1">
 	</div>
 	<div class="input-group">
-		<span class="input-group-addon" id="basic-addon1">Address 2:</span>
-		<input type="text" class="form-control" placeholder="Address 2" aria-describedby="basic-addon1">
-	</div>
-	<div class="input-group">
 		<span class="input-group-addon" id="basic-addon1">City:</span>
 		<input type="text" class="form-control" placeholder="City" aria-describedby="basic-addon1">
 	</div>
@@ -83,10 +79,6 @@ $this->load->view('partials/userHeader');
 	<div class="input-group">
 		<span class="input-group-addon" id="basic-addon1">Address:</span>
 		<input type="text" class="form-control" placeholder="Address" aria-describedby="basic-addon1">
-	</div>
-	<div class="input-group">
-		<span class="input-group-addon" id="basic-addon1">Address 2:</span>
-		<input type="text" class="form-control" placeholder="Address 2" aria-describedby="basic-addon1">
 	</div>
 	<div class="input-group">
 		<span class="input-group-addon" id="basic-addon1">City:</span>
