7fa3adef74ef5f8b917a1d33915414545f0f804e
Justin Pfeiffer <pfeiffer.justin.pfeiffer@gmail.com>
2016-09-28 10:10:34 -0700

    push branch
    wdfwefwef

---
+++ application/views/index.php
@@ -0,0 +1,33 @@
+<?php $this->load->view('partials/head'); 
+$this->load->view('partials/userHeader')?>
+
+<div class="container">
+	<div class="jumbotron" id="banner">
+		<h1>BootCamp Supply Store</h1>
+		<p>We have all you need to start your very own coding bootcamp.</p>
+		<p><a class="btn btn-success btn-lg" href="store" role="button">Start Shopping</a></p>
+	</div>
+	<div class="row">
+	  <div class="col-xs-6 col-md-3">
+	    <a href="#" class="thumbnail">
+	      <img src="..." alt="...">
+	    </a>
+	  </div>
+	  <div class="col-xs-6 col-md-3">
+	    <a href="#" class="thumbnail">
+	      <img src="..." alt="...">
+	    </a>
+	  </div>
+	  <div class="col-xs-6 col-md-3">
+	    <a href="#" class="thumbnail">
+	      <img src="..." alt="...">
+	    </a>
+	  </div>
+	  <div class="col-xs-6 col-md-3">
+	    <a href="#" class="thumbnail">
+	      <img src="..." alt="...">
+	    </a>
+	  </div>
+	</div>
+</div>
+
---
+++ application/views/store.php
@@ -0,0 +1,2 @@
+<?php $this->load->view('partials/head'); 
+$this->load->view('partials/userHeader')?>
