519b5a06662a343b4a1a7afe6c10545769badc0f
Timothy Lo <tim7643@gmail.com>
2016-09-28 12:40:38 -0700

    fixed some images

--- application/views/index.php
+++ application/views/index.php
@@ -10,22 +10,22 @@ $this->load->view('partials/userHeader')?>
 	<div class="row">
 		<div class="col-xs-6 col-md-3">
 			<a href="#" class="thumbnail">
-				<img src="..." alt="...">
+			<img src="placeholder-thumb.jpg" alt="...">
 			</a>
 		</div>
 		<div class="col-xs-6 col-md-3">
 			<a href="#" class="thumbnail">
-				<img src="..." alt="...">
+				<img src="placeholder-thumb.jpg" alt="...">
 			</a>
 		</div>
 		<div class="col-xs-6 col-md-3">
 			<a href="#" class="thumbnail">
-				<img src="..." alt="...">
+				<img src="placeholder-thumb.jpg" alt="...">
 			</a>
 		</div>
 		<div class="col-xs-6 col-md-3">
 			<a href="#" class="thumbnail">
-				<img src="..." alt="...">
+				<img src="placeholder-thumb.jpg" alt="...">
 			</a>
 		</div>
 	</div>
