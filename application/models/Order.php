41597f71713eeafdc502529c4403c9b0b6bf9fe0
Justin Pfeiffer <pfeiffer.justin.pfeiffer@gmail.com>
2016-09-28 11:46:58 -0700

    storefront update
    update cart and store

--- application/models/Order.php
+++ application/models/Order.php
@@ -2,12 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Order extends CI_Model {
+<<<<<<< Updated upstream
 	public function showCart() {
 		// use session data
 	}
 	public function recordInfo() {
 		// add in user info
 	}
+=======
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
 
 
 
--- application/models/Product.php
+++ application/models/Product.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Product extends CI_Model {
+<<<<<<< Updated upstream
 	public function getItemDetails() {
 		$sql="SELECT name, price, img, description FROM products WHERE";
 
@@ -9,6 +10,10 @@ class Product extends CI_Model {
 	public function getSimilarItems() {
 		// get pic name and price for items in the same category
 	}
+=======
+
+
+>>>>>>> Stashed changes
 
 
 
