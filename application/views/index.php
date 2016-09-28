cc2874b641f4ad8b42e15539a82cf4893e668eb9
Justin Pfeiffer <pfeiffer.justin.pfeiffer@gmail.com>
2016-09-28 12:48:59 -0700

    Style Updates
    finished store front styling.

--- application/views/index.php
+++ application/views/index.php
@@ -1,33 +1,38 @@
 <?php $this->load->view('partials/head'); 
 $this->load->view('partials/userHeader')?>
-
 <div class="container">
-	<div class="jumbotron" id="banner">
-		<h1>BootCamp Supply Store</h1>
-		<p>We have all you need to start your very own coding bootcamp.</p>
-		<p><a class="btn btn-success btn-lg" href="store" role="button">Start Shopping</a></p>
-	</div>
-	<div class="row">
-		<div class="col-xs-6 col-md-3">
-			<a href="#" class="thumbnail">
-			<img src="/assets/img/placeholder-thumb.jpg" alt="...">
-			</a>
+	<div class="col-md-12">
+		<div class="jumbotron" id="banner">
+			<h1>BootCamp Supply Store</h1>
+			<p>We have all you need to start your very own coding bootcamp.</p>
+			<p><a class="btn btn-success btn-lg" href="store" role="button">Start Shopping</a></p>
 		</div>
-		<div class="col-xs-6 col-md-3">
-			<a href="#" class="thumbnail">
-				<img src="/assets/img/placeholder-thumb.jpg" alt="...">
-			</a>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<a href="#" class="thumbnail">
-				<img src="/assets/img/placeholder-thumb.jpg" alt="...">
-			</a>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<a href="#" class="thumbnail">
-				<img src="/assets/img/placeholder-thumb.jpg" alt="...">
-			</a>
+		<div class="row">
+			<div class="col-xs-6 col-md-3">
+				<a href="#" class="thumbnail">
+					<img src="/assets/img/placeholder-thumb.jpg" alt="category image">
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<a href="#" class="thumbnail">
+					<img src="/assets/img/placeholder-thumb.jpg" alt="category image">
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<a href="#" class="thumbnail">
+					<img src="/assets/img/placeholder-thumb.jpg" alt="category image">
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<a href="#" class="thumbnail">
+					<img src="/assets/img/placeholder-thumb.jpg" alt="category image">
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
+<script type="text/javascript">
+	
+			$(".fullBackground").css("height", $(window).height());
 
+</script>
