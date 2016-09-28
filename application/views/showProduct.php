44ab297c92f40258254f86fda5b085697af2d8be
Timothy Lo <tim7643@gmail.com>
2016-09-28 11:52:42 -0700

    semi colon

--- application/views/showProduct.php
+++ application/views/showProduct.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$this->load->view("partials/head")
+$this->load->view("partials/head");
 $this->load->view('partials/userHeader');
 ?>
 
