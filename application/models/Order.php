2709624e30db4d6703e0d5809cbfa290890ea225
Timothy Lo <tim7643@gmail.com>
2016-09-29 08:46:20 -0700

    admin order sql statement

--- application/models/Order.php
+++ application/models/Order.php
@@ -29,7 +29,7 @@ class Order extends CI_Model {
 	}
 	public function getAllOrders()
 	{
-		return $this->db->query("SELECT orders.id, users.first_name, users.last_name, orders.created_at, street, city, state, zip FROM orders JOIN users ON orders.user_id=users.id JOIN billing ON users.billing_id=billing.id JOIN addresses ON billing_address_id=addresses.id")->result_array();
+		return $this->db->query("SELECT orders.id, users.first_name, users.last_name, orders.created_at, street, city, state, zip FROM orders JOIN users ON orders.user_id=users.id JOIN billing ON users.billing_id=billing.id JOIN addresses ON billing_address_id=addresses.id JOIN order_products ON order_products.order_id = orders.id JOIN products ON products.id=order_products.product.id")->result_array();
 	}
 
 }
