bdda50e605848583ede7f3a9477d41c1382c090a
Timothy Lo <tim7643@gmail.com>
2016-09-29 08:38:57 -0700

    made temp link

--- application/views/partials/userHeader.php
+++ application/views/partials/userHeader.php
@@ -15,6 +15,7 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
+					<li><a href="/AdminOrders">[temp admin link]</a></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="/goToCart">Shopping Cart (<?= count($this->session->cart) ?>) <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
